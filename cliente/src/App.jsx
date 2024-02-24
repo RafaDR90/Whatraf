0ee7836e3ff6bfa8f8d79d3e19948802c719e5d1
RafaelDR <rafa18220delgado@gmail.com>
2024-02-24 10:25:13 +0100

    24 02

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from 'react-router-dom';
 export default function App() {
   const [nombre, setNombre] = useState('');
   const navigate = useNavigate();
+  const fileInput = document.getElementById('fileInput');
+  const endpoint = 'http://localhost:3000/upload';
 
   function asignaNombre() {
     if (nombre.trim() !== '') {
@@ -13,6 +15,25 @@ export default function App() {
     } else {
       console.log('Por favor, ingresa un nombre válido.');
     }
+  }
+
+  function insertaImagen() {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+    const formData = new FormData();
+    formData.append('sampleFile', file);
+    fetch(endpoint, {
+      method: 'POST',
+      body: formData
+    })
+    .then(data => {
+      console.log(data);
+      alert('File uploaded successfully!');
+    })
+    .catch(error => {
+      console.error(error);
+      alert('Error uploading file');
+    });
   }
 
   useEffect(() => {
@@ -54,6 +75,10 @@ export default function App() {
       <h2>Bienvenido, escribe tu nombre para continuar:</h2>
       <input type='text' value={nombre} onChange={(e) => setNombre(e.target.value)} />
       <button onClick={asignaNombre}>Enviar</button>
+      <form>
+      <input type="file" id="fileInput" name="fileInput"></input>
+      <button type="button" onClick={insertaImagen} id="uploadButton">Upload</button>
+      </form>
     </div>
   );
 }
--- servidor/app.js
+++ servidor/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+const fileUpload = require('express-fileupload');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -13,8 +14,36 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.post('/upload', function(req, res) {
+    let sampleFile;
+    let uploadPath;
+  
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+  
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    sampleFile = req.files.sampleFile;
+    uploadPath = __dirname + '/public/fotos/' + sampleFile.name;
+  
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(uploadPath, function(err) {
+      if (err)
+        return res.status(500).send(err);
+  
+      res.send('File uploaded!');
+    });
+  });
 
 module.exports = app;
